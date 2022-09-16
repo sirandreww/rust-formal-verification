043e36c6a71718a81c51f45722544114c103991e
sirandreww <andrew.luka.ia@gmail.com>
2022-09-16 10:38:10 +0300

    adding to doc

--- src/formulas/cnf.rs
+++ src/formulas/cnf.rs
@@ -22,6 +22,53 @@ pub struct CNF {
 // ************************************************************************************************
 
 impl CNF {
+    /// Function that adds a clause to the CNF.
+    /// If the clause already exists then it is not added.
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - a mut reference to self.
+    /// * `new_clause` - an immutable reference to a Clause you want to add.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rust_formal_verification::formulas::CNF;
+    /// use rust_formal_verification::formulas::Clause;
+    /// use rust_formal_verification::formulas::Literal;
+    /// use rust_formal_verification::formulas::Variable;
+    ///
+    /// let v1 = Variable::new(1);
+    /// let v2 = Variable::new(2);
+    /// let v3 = Variable::new(3);
+    ///
+    /// let l1 = Literal::new(v1, false);
+    /// let l2 = Literal::new(v2, false);
+    /// let l3 = Literal::new(v3, false);
+    ///
+    /// let c1 = Clause::new(vec![l1, l2, l3]);
+    /// let c2 = Clause::new(vec![!l1, l2, l3]);
+    /// let c3 = Clause::new(vec![l1, !l2, l3]);
+    /// let c4 = Clause::new(vec![l1, l2, !l3]);
+    ///
+    /// let mut cnf1 = CNF::default();
+    ///
+    /// cnf1.add_clause(&c1);
+    /// cnf1.add_clause(&c2);
+    /// cnf1.add_clause(&c3);
+    /// cnf1.add_clause(&c4);
+    ///
+    /// assert_eq!(cnf1.to_string(), "((!x1 | x2 | x3) & (x1 | !x2 | x3) & (x1 | x2 | !x3) & (x1 | x2 | x3))");
+    ///
+    /// let mut cnf2 = CNF::default();
+    /// assert_eq!(cnf2.to_string(), "()");
+    /// cnf2.add_clause(&(c1.clone()));
+    /// assert_eq!(cnf2.to_string(), "((x1 | x2 | x3))");
+    /// cnf2.add_clause(&(c1.clone()));
+    /// // Notice how the CNF did not change upon adding an identical copy of a clause that already exists.
+    /// assert_eq!(cnf2.to_string(), "((x1 | x2 | x3))");
+    ///
+    /// ```
     pub fn add_clause(&mut self, new_clause: &Clause) {
         self.max_variable_number = max(
             self.max_variable_number,
@@ -42,7 +89,7 @@ impl CNF {
     ///
     /// # Arguments
     ///
-    /// None
+    /// reference to self
     ///
     /// # Examples
     ///
