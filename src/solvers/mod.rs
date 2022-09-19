12875d79b754f0919ee5d7e23899d563f3b40c9c
sirandreww <andrew.luka.ia@gmail.com>
2022-09-20 00:03:26 +0300

    working on SAT solving

--- src/solvers/mod.rs
+++ src/solvers/mod.rs
@@ -3,11 +3,9 @@
 // ************************************************************************************************
 
 pub mod minisat;
-pub mod sat_solver; // requires file in this directory with the name 'minisat.rs'
 
 // ************************************************************************************************
 // re-exports of structs in these modules to simplify paths for other imports
 // ************************************************************************************************
 
 pub use minisat::MinisatSolver;
-pub use sat_solver::SatSolver;
