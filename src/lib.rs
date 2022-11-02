c28832f152a632c37923d606781d1484acc12277
Andrew Luka <andrew.luka.ia@gmail.com>
2022-11-03 00:08:47 +0200

    added description and fixed stated license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+/// This crate is for developing and prototyping algorithms for formal verification for hardware
+/// devices. Algorithms like BMC, IC3, PDR and so on...
+
 // ************************************************************************************************
 // rust submodule declaration, they get searched in their respective file  names
 // ************************************************************************************************
@@ -5,7 +8,6 @@
 pub mod formulas; // requires existence of 'formulas/mod.rs'
 pub mod models; // requires existence of 'models/mod.rs'
 pub mod solvers; // requires existence of 'solvers/mod.rs'
-                 // pub mod traits; // requires existence of 'traits/mod.rs'
 
 // ************************************************************************************************
 // re-exports of structs in these modules to simplify paths for other imports
