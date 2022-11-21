a3245758f370a2aa7a226bc9729bb29f16aca0e1
Andrew Luka <andrew.luka.ia@gmail.com>
2022-11-21 14:42:10 +0200

    help files updated

--- tests/examples/help.py
+++ tests/examples/help.py
@@ -21,8 +21,11 @@ def get_all_file_paths_in_dir_recursively(rootdir: str):
             result += [file_path]
     return result
 
+def is_path_aig(file_path):
+    return file_path[-4:] == ".aig"
+
 def is_path_aig_and_not_folded(file_path):
-    return (file_path[-4:] == ".aig") and ("_folded" not in file_path)
+    return is_path_aig(file_path) and ("_folded" not in file_path)
 
 def run_cmd(cmd: str):
     print(cmd)
@@ -37,21 +40,21 @@ functions
 
 def convert_aigs_to_aag():
     for file_path in get_all_file_paths_in_dir_recursively("./"):
-        if is_path_aig_and_not_folded(file_path=file_path):
+        if is_path_aig(file_path=file_path):
             run_cmd(f"./aigtoaig {file_path} {file_path[:-4]}.aag")
-            
-            
+
 def find_unsafe_tests():
     pass
 
-def unfold_aigs():
+def zero_then_fold_aigs():
     for file_path in get_all_file_paths_in_dir_recursively("./hwmcc20/"):
         if is_path_aig_and_not_folded(file_path=file_path):
             assert(file_path[-4:] == ".aig")
-            out_file = f"{file_path[:-4]}_folded.aig"
+            out_file = f"{file_path[:-4]}_zeroed_then_folded.aig"
             print(out_file)
             run_cmd(f'./abc -c "read {file_path}; zero ; fold2 ; write_aiger {out_file}"')
-            # return
+    # make aag files for these new aig files
+    convert_aigs_to_aag()
 
 """
 ***************************************************************************************************
@@ -60,4 +63,4 @@ call depending on need
 """
 
 if __name__ == "__main__":
-    convert_aigs_to_aag()
+    zero_then_fold_aigs()
